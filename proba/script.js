fbb0da348ef0eb78562fd202481a9d93fc0ed535
Chris-vd <a24christianvd@iessanclemente.net>
2025-09-23 11:01:09 +0200

    clase

--- proba/script.js
+++ proba/script.js
@@ -1,5 +1,16 @@
-console.log("erm");
-let num = 4;
-console.log(num);
-console.log
+'use strict';
 
+// x &&= y -> x && (x == y)
+let x = 0;
+let y = 1;
+console.log((x &&= 0), x); // x && (x == 0) true -> x -> 0
+console.log((x &&= 1), x); // x && (X == 1) false -> x -> 0
+console.log((y &&= 1), y); // y && (y == 1) true -> (y == 1) -> 1
+console.log((y &&= 0), y); // y && (y == 0) false -> (y == 0) -> 0
+
+x = 0;
+y = 1;
+console.log((x ||= 0), x); // x || (x == 0) true -> (x == 0) -> 0
+console.log((x ||= 1), x); // x || (x == 1) false -> (x == 1) -> 1
+console.log((y ||= 1), y); // y || (y == 1) true -> y -> 1
+console.log((y ||= 0), y); // y || (y == 0) false -> y -> 1
