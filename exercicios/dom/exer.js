1cafb00c0bead1d62e7990d9d0f3d51d36697976
Chris-VD <a24christianvd@iessanclemente.net>
2025-10-20 20:23:11 +0200

    DOM exer CC

---
+++ exercicios/dom/exer.js
@@ -0,0 +1,16 @@
+console.log(document.getElementById("age-table"));
+console.log(document.getElementById("age-table").getElementsByTagName("label"));
+console.log(document.getElementById("age-table").querySelector("td"));
+console.log(document.getElementsByTagName("form").search);
+console.log(document.getElementsByTagName("form").search.querySelector("input"));
+console.log(document.getElementsByTagName("form").search.querySelector("input"));
+let form = document.getElementsByTagName("form").search.querySelectorAll("input");
+console.log(form[form.length-1]);
+
+console.log(document.getElementById("input2"));
+console.log(document.querySelectorAll("p"));
+console.log(document.getElementById("lipsum").querySelectorAll("p"));
+console.log(document.querySelector("form"));
+console.log(document.getElementsByTagName("input"));
+console.log(document.getElementsByTagName("input").sexo);
+console.log(document.getElementsByClassName("important"));
