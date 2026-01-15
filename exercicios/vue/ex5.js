b8e62d4c95bec0a24a499c499ff29c2c78e52fff
Chris-vd <a24christianvd@iessanclemente.net>
2026-01-15 11:10:01 +0100

    cc

---
+++ exercicios/vue/ex5.js
@@ -0,0 +1,28 @@
+const {createApp} = Vue;
+createApp({
+    data(){
+        return {
+            first: "first",
+            cl: "green"
+        }
+    },
+    methods:{
+        ff(event){
+            event.target.classList.toggle(this.first)
+        },
+        ss(){
+            if (this.cl == "green") this.cl = "red"
+            else this.cl = "green"
+        },
+        change(event){
+            event.target.classList.toggle("activa")
+        },
+        changeOne(event){
+            let list = event.currentTarget.children;
+            for (div in Array.from(list)){
+                if (Array.from(list[div].classList).includes("activa")) list[div].classList.toggle("activa")
+            }
+            event.target.classList.toggle("activa")
+        }
+    }
+}).mount("#app")
